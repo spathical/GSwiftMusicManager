43d7f759a5e7c619f588e1b8d50cd5a6de867429
santhosh <santhosh@shopcurbside.com>
2017-07-09 12:54:50 -0700

    Initial Commit

---
+++ GSwiftMusicManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GSwiftMusicManager
+//
+//  Created by Santhosh Pathical on 7/9/17.
+//  Copyright © 2017 Iam Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
